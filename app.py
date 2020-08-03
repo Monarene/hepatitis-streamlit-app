8e7ec6fdf08f029af994020b71a79bc51646f4b0
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-08-03 06:57:53 +0100

    started writing the streamlit app

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import streamlit as st 
+import numpy as np
+import pandas as pd
+from PIL import Image
+import pickle
+
+# import the machine learning algorithm
+pickle_in = open("hepatitis_random_forest.pkl", "rb")
+classifier = pickle.load(pickle_in)
+
+def predict_hepatitis(age, sex, malaise, anorexia, liver_big, liver_firm, ascites, histology):
+    prediction = classifier.predict([[age, sex, malaise, anorexia, liver_big, liver_firm, ascites, histology]])
+    print(prediction)
+    return prediction 
 
 def main():
-	""" A simple Iris EDA App """
+	""" Hepatitis B Detector App """
 
-	st.title("Iris EDA App with streamlit")
-	st.subheader("Streamlit is Cool")
+ 	html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Hepatitis B Detector App </h2>
+    </div>
+    """
 
 if __name__ == '__main__':
 	main()
