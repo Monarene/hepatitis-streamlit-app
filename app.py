5a1d3799265580b5b0fdbd426bbaf0420dede281
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-08-03 15:08:34 +0100

    works

--- app.py
+++ app.py
@@ -23,14 +23,52 @@ def main():
     """
     st.markdown(html_temp,unsafe_allow_html=True)
     age = st.text_input("Age (Age in years)")
-    sex = st.text_input("Sex (1=Male, 2=Female)")
-    malaise = st.text_input("Malaise (Discomfort 1=Yes, 2=No)")
-    anorexia = st.text_input("Anorexis (Vomiting 1=Yes, 2=No)")
-    liver_big = st.text_input("Enlarged Liver due to Liver diseases(1=Yes, 2=No)")
-    liver_firm = st.text_input("Firm Liver due to liver diseases(1=Yes, 2=No)")
-    ascites = st.text_input("Excess Abdominal fluid > 25ml of fluid in the peritoneal(1=Yes, 2=No)")
-    histology = st.text_input("History with the disease(1=Yes, 2=No)")
+    sex = st.text_input("Sex (Male or Female)")
+    malaise = st.text_input("Malaise (Discomfort  Yes or No)")
+    anorexia = st.text_input("Anorexis (Vomiting Yes or No)")
+    liver_big = st.text_input("Enlarged Liver due to Liver diseases(Yes or No)")
+    liver_firm = st.text_input("Firm Liver due to liver diseases(Yes or No)")
+    ascites = st.text_input("Excess Abdominal fluid > 25ml of fluid in the peritoneal(Yes or No)")
+    histology = st.text_input("History with the disease(Yes or No)")
+    
+        
+    if sex.lower() == "male":
+        sex = 1
+    elif sex.lower() == "female":
+        sex = 2
+        
+    if malaise.lower() == "yes":
+        malaise = 1
+    elif malaise.lower() == "no":
+        malaise = 2
+        
+    if anorexia.lower() == "yes":
+        anorexia = 1
+    elif anorexia.lower() == "no":
+        anorexia = 2
+        
+    if liver_big.lower() == "yes":
+        liver_big = 1
+    elif liver_big.lower() == "no":
+        liver_big = 2
+        
+    if liver_firm.lower() == "yes":
+        liver_firm = 1
+    elif liver_firm.lower() == "no":
+        liver_firm = 2
+        
+    if ascites.lower() == "yes":
+        ascites = 1
+    elif ascites.lower() == "no":
+        ascites = 2
+        
+    if histology.lower() == "yes":
+        histology = 1
+    elif histology.lower() == "no":
+        histology = 2    
+    
     result  = ""
+    
     if st.button("Predict"):
         result=predict_hepatitis(age, sex, malaise, anorexia, liver_big, liver_firm, ascites, histology)
     if result == 1:
